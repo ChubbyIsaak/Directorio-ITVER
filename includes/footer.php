bfe2d531d08e11ad643735df608cf6db9a2909c1
ChubbyIsaak <chubbyisaac908@gmail.com>
2023-05-23 14:08:13 -0600

    HeyBoss

---
+++ includes/footer.php
@@ -0,0 +1,17 @@
+    </main>
+    <link rel="stylesheet" href="doc/__static/normalize.css">
+    <link rel="shortcut icon" href="doc/__images/favicon.png"
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Krub&family=Staatliches&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="doc/__static/styles.css">
+    <script src="/Directorio ITVER/bt/js/bootstrap.js"></script>
+    <script src="/Directorio ITVER/dt/jq.js"></script>
+    <script type="text/javascript" src="/Directorio ITVER/dt/datatables.min.js"></script>
+    <script>
+        $(document).ready( function () {
+            $('#misdatos').DataTable();
+        } );
+    </script>
+</body>
+</html>
